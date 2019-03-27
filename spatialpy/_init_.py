689b444fdd43ef94a4437d6d6d46a0f05b9c9cc7
Evie Hilton <42820134+eviehilton@users.noreply.github.com>
2019-03-27 09:41:39 -0400

    Update _init_.py

--- spatialpy/_init_.py
+++ spatialpy/_init_.py
@@ -1,2 +1,12 @@
 #__all__=['model','spatialpy']
 from .spatialpy import *
+
+import sys
+if (sys.version_info < (3,0)):
+    raise Exception("SpatialPy only works in Python 3.0 and higher")
+
+
+#from spatialpy.core import *
+#import spatialpy.sbml
+#import spatialpy.solvers
+#import spatialpy.example_models
