36c21d45c2c1b02dd9f1d6dad4974173ba7ccaeb
makdl <31526990+makdl@users.noreply.github.com>
2020-07-06 12:49:18 -0400

    Removed print statements

--- spatialpy/Solver.py
+++ spatialpy/Solver.py
@@ -73,8 +73,6 @@ class Solver:
         makefile = self.SpatialPy_ROOT+'/build/Makefile'
         cmd = " ".join(['cd', self.build_dir, ';', 'make', '-f', makefile, 'ROOT=' +
                         self.SpatialPy_ROOT, 'MODEL=' + self.prop_file_name, 'BUILD='+self.build_dir, 'VERB='+str(self.debug_level)])
-        print("HERE")
-        print(cmd)
 
         if log.isEnabledFor(logging.DEBUG):
             log.debug("cmd: {0}\n".format(cmd))
