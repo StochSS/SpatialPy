9ac94cf91b6f16be1168fbf4e107ac51be299a97
makdl <31526990+makdl@users.noreply.github.com>
2020-07-16 12:19:25 -0400

    Reworked macros, may change again

--- spatialpy/ssa_sdpd-c-simulation-engine/include/debug.h
+++ spatialpy/ssa_sdpd-c-simulation-engine/include/debug.h
@@ -6,11 +6,11 @@
 #if DEBUG_LEVEL > 0
     // http://support.raisonance.com/content/how-remove-file-path
     #define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
-    #define SSA_LOG(setlevel, ...) \
-        do { ssa_printf(setlevel, DEBUG_LEVEL, __DATE__, __TIME__, __FILENAME__, __LINE__, ## __VA_ARGS__); } while(0)
+    #define SSA_LOG(level, fmt, ...) \
+        do { if (DEBUG_LEVEL >= level) \
+            printf("%s %s - ssa-sdpd.%s - %s - %i - " fmt, __DATE__, __TIME__, __FILENAME__, ssa_level(level), __LINE__, ##__VA_ARGS__); } while(0)
 #else
-    #define SSA_LOG(setlevel, fmt, ...)
+    #define SSA_LOG(level, fmt, ...)
 #endif
 
-const char *ssa_level(int setlevel);
-extern void ssa_printf(int setlevel, int debuglevel, const char *date, const char* time, const char *filename, int line, const char *fmt, ...);
+const char *ssa_level(int level);
--- spatialpy/ssa_sdpd-c-simulation-engine/src/debug.c
+++ spatialpy/ssa_sdpd-c-simulation-engine/src/debug.c
@@ -1,23 +1,12 @@
 #include "debug.h"
 
-const char *ssa_level(int setlevel){
-    switch (setlevel){
+const char *ssa_level(int level){
+    switch (level){
         case 1:
             return "INFO";
         case 2:
             return "DEBUG";
         default:
             return "UNKNOWN LEVEL";
-    }
-}
-
-extern void ssa_printf(int setlevel, int debuglevel, const char *date, const char *time, const char *filename, int line, const char *fmt, ...){
-    if (debuglevel >= setlevel){
-        va_list vaargs;
-        fprintf(stdout, "%s %s - ssa-sdpd.%s - %s - %i - ", date, time, filename, ssa_level(setlevel), line);
-        va_start(vaargs, fmt);
-        vfprintf(stdout, fmt, vaargs);
-        va_end(vaargs);
-        fflush(stdout);
     }
 }
