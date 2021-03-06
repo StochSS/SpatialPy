6b7ba2e63169aad1f31823b1cd9f1c6f9ca052c0
makdl <31526990+makdl@users.noreply.github.com>
2021-03-05 23:40:07 -0500

    Updated URLS & license clarification

--- spatialpy/__version__.py
+++ spatialpy/__version__.py
@@ -2,14 +2,14 @@
 # @file    __version__.py
 # @brief   SpatialPy version info
 # @license Please see the file named LICENSE in the project directory
-# @website https://github.com/SpatialPy/SpatialPy
+# @website https://github.com/StochSS/SpatialPy
 # =============================================================================
 
 __version__      = '0.5.0'
 __title__        = 'SpatialPy'
 __description__  = 'Python Interface for Spatial Stochastic Biochemical Simulations'
 __url__          = 'https://spatialpy.github.io/SpatialPy/'
-__download_url__ = 'https://github.com/SpatialPy/SpatialPy/tarball/master/'
+__download_url__ = 'https://github.com/StochSS/SpatialPy/tarball/main/'
 __author__       = 'See AUTHORS'
 __email__        = 'bdrawert@unca.edu'
 __license__      = 'GPL-3.0-only'
