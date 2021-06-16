c6ec51a350302f699bfffe9c2f3a8270d0fa45b0
Brian Drawert <brian@drawert.net>
2021-06-16 12:42:55 -0400

    testing

--- spatialpy/ssa_sdpd-c-simulation-engine/src/output.c
+++ spatialpy/ssa_sdpd-c-simulation-engine/src/output.c
@@ -61,7 +61,10 @@ namespace Spatialpy{
             p = &system->particles[i] ;
             memcpy( (void *) &output_buffer[ncnt++], (void *) p, sizeof(Particle) );
             if(system->num_chem_species > 0){
-                memcpy( (void *) &output_buffer_chem[p->id*system->num_chem_species], (void*) (&p->C), sizeof(double)*system->num_chem_species );
+                //memcpy( (void *) &output_buffer_chem[p->id*system->num_chem_species], (void*) (&p->C), sizeof(double)*system->num_chem_species );
+                for(int s = 0 s < system->num_chem_species; s++){
+                    output_buffer_chem[p->id*system->num_chem_species + s] = p->C[s]
+                }
             }
         }
         output_buffer_current_num_particles = ncnt;
