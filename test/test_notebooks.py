638e0b5125a8e9dd5c828e97c9b0dbb505d3b596
seanebum <smatthe2@unca.edu>
2021-10-27 14:52:07 -0400

    cleaned up paths

--- test/test_notebooks.py
+++ test/test_notebooks.py
@@ -31,7 +31,8 @@ class TestNotebooks(unittest.TestCase):
         errors = {}
         ep = ExecutePreprocessor(timeout=600, kernel_name='python3', allow_errors=True)
         if test_set == FULL:
-            for root, dirs, files in os.walk("../examples/"):
+            test_dir = os.path.join('..', 'examples')
+            for root, dirs, files in os.walk(test_dir):
                 for file in files:
                     if file.endswith(".ipynb"):
                          with open(os.path.join(root, file)) as f:
@@ -43,18 +44,19 @@ class TestNotebooks(unittest.TestCase):
                                 print('Error executing the notebook "{}".\n\n'.format(file))
                                 errors[file] = err
         elif test_set == MINIMAL:
-            files = ['cylinderDemo/SpatialPy_cylinderDemo3D.ipynb', 'tests/Diffusion_validation.ipynb',
-                        'tests/Spatial_Birth_Death.ipynb']
-            root = '../examples'
-                for file in files:
-                     with open(os.path.join(root, file)) as f:
-                        print('Executing {}...'.format(file))
-                        nb = nbformat.read(f, as_version=nbformat.NO_CONVERT)
-                        try:
-                            ep.preprocess(nb, {'metadata': {'path': root}})
-                        except Exception as err:
-                            print('Error executing the notebook "{}".\n\n'.format(file))
-                            errors[file] = err
+            files = [os.path.join('cylinderDemo', 'SpatialPy_cylinderDemo3D.ipynb'), 
+                        os.path.join('tests', 'Diffusion_validation.ipynb'),
+                        os.path.join('tests', 'Spatial_Birth_Death.ipynb']
+            root = os.path.join('..', 'examples') 
+            for file in files:
+                 with open(os.path.join(root, file)) as f:
+                    print('Executing {}...'.format(file))
+                    nb = nbformat.read(f, as_version=nbformat.NO_CONVERT)
+                    try:
+                        ep.preprocess(nb, {'metadata': {'path': root}})
+                    except Exception as err:
+                        print('Error executing the notebook "{}".\n\n'.format(file))
+                        errors[file] = err
         for fname, err in errors.items():
             if len(err.__str__()) > 500:
                 print('{}:\n{}\n...\n{}'.format(fname, err.__str__()[:251], err.__str__()[-251:]))
