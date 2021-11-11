9464777ec44ea43f88887c67ec0d96d9e5c41e1e
Bryan Rumsey <bmrumsey@gmail.com>
2021-11-11 08:18:36 -0500

    Fixed the core import.

--- spatialpy/__init__.py
+++ spatialpy/__init__.py
@@ -26,5 +26,5 @@ from .__version__ import __version__, __title__, __description__, __url__
 from .__version__ import __author__, __email__
 from .__version__ import __license__, __copyright__
 
-from gillespy2.core import *
+from spatialpy.core import *
 from spatialpy.Solver import *
