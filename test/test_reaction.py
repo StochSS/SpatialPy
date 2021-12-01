0daf7a587546e6a3e8114bd2c604b91b56964a39
Bryan Rumsey <bmrumsey@gmail.com>
2021-12-01 11:55:54 -0500

    Added missing re import.

--- test/test_reaction.py
+++ test/test_reaction.py
@@ -15,6 +15,7 @@ GNU GENERAL PUBLIC LICENSE Version 3 for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
+import re
 import unittest
 
 import spatialpy
