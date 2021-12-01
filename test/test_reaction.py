38301779fdf58611663a99f9c5efe55c46de2652
Bryan Rumsey <bmrumsey@gmail.com>
2021-12-01 11:02:21 -0500

    Added missing import for reaction tests.

--- test/test_reaction.py
+++ test/test_reaction.py
@@ -18,7 +18,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import unittest
 
 import spatialpy
-from spatialpy import Reaction
+from spatialpy import Reaction, Parameter
 from spatialpy import ReactionError
 
 class TestReaction(unittest.TestCase):
