4c6c1ec1bf6fc91cd6a9cedf04569d05b279dd31
Bryan Rumsey <bmrumsey@gmail.com>
2022-02-10 10:06:08 -0500

    Pylint changes.

--- spatialpy/stochss/__init__.py
+++ spatialpy/stochss/__init__.py
@@ -15,4 +15,3 @@ GNU GENERAL PUBLIC LICENSE Version 3 for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
-
