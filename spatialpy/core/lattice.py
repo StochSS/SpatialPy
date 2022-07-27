6c45d5f6a4154649d566ada94e15206f8fb15128
Bryan Rumsey <bmrumsey@gmail.com>
2022-07-27 16:17:32 -0400

    Added checks to apply function to ensure proper args are passed.  Added spherical lattice.

--- spatialpy/core/lattice.py
+++ spatialpy/core/lattice.py
@@ -15,6 +15,8 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import numpy
 
+from spatialpy.core.domain import Domain
+from spatialpy.core.Geometry import Geometry, CombinatoryGeometry
 from spatialpy.core.spatialpyerror import LatticeError
 
 
@@ -22,7 +24,7 @@ class Lattice:
 	"""
     Lattice class provides a method for creating parts of the spatial domain.
 
-    :param center: The center point of the lattice.
+    :param center: The center point of the lattice (optional, defaults to [0, 0, 0]).
     :type center: float[3] | float(3)
 
     :raises LatticeError: if center is not and list, doesn't contain 3 values,
@@ -41,15 +43,14 @@ class Lattice:
 
     def apply(self, domain, geometry, transform=None, **kwargs):
     	"""
-    	Fill a domain with particles within the lattice and geometry.
+    	Fill a domain with particles within the lattice restricted by the geometry.
 
     	:param domain: Domain particles are to be added to.
     	:type domain: spatialpy.Domain
 
     	:param geometry: Geometry defining the region within the lattice in
     		which particles are restricted to.
-    	:type geometry: spatialpy.Geometry | spatialpy.CombinatoryGeometry |
-    		spatialpy.Transformation
+    	:type geometry: spatialpy.Geometry | spatialpy.CombinatoryGeometry
 
     	:param transform: Transformation function applied to each particle.
     	:type transform: function
@@ -136,9 +137,46 @@ class CartesianLattice(Lattice):
 
     	self.validate()
 
-    def apply():
+    def apply(self, domain, geometry, transform=None, **kwargs):
+    	"""
+    	Fill a domain with particles within the cartesian lattice restricted by the geometry.
 
-    def validate():
+    	:param domain: Domain particles are to be added to.
+    	:type domain: spatialpy.Domain
+
+    	:param geometry: Geometry defining the region within the lattice in
+    		which particles are restricted to.
+    	:type geometry: spatialpy.Geometry | spatialpy.CombinatoryGeometry
+
+    	:param transform: Transformation function applied to each particle.
+    	:type transform: function
+
+    	:param \**kwargs: Additional keyword arguments passed to :py:meth:`Domain.add_point`.
+    	"""
+    	if not (isinstance(domain, Domain) or type(domain).__name__ == 'Domain'):
+    		raise LatticeError("domain must be of type spatialpy.Domain.")
+    	if not (isinstance(geometry, (Geometry, CombinatoryGeometry)) or \
+    		type(geometry).__name__ in ('Geometry', 'CombinatoryGeometry')):
+    		raise LatticeError(
+    			"geometry must be of type spatialpy.Geometry or spatialpy.CombinatoryGeometry."
+    		)
+    	if transform is not None and not isinstance(transform, 'function'):
+    		raise LatticeError("transform must be a function.")
+
+    	count = 0
+        for x in numpy.arange(self.xmin, self.xmax + self.deltax, self.deltax):
+            for y in numpy.arange(self.ymin, self.ymax + self.deltay, self.deltay):
+                for z in numpy.arange(self.zmin, self.zmax + self.deltaz, self.deltaz):
+                    if geometry.inside((x, y, z), False):
+                        if transform is None:
+                        	point = [x, y, z]
+                        else:
+                        	point = transform([x, y, z])
+                        domain.add_point(point, **kwargs)
+                        count += 1
+        return count
+
+    def validate(self):
     	"""
     	Validate the cartesian lattice dependencies.
     	"""
@@ -162,13 +200,121 @@ class CartesianLattice(Lattice):
 			raise LatticeError("zmax must be greater than zmin.")
 		if not isinstance(deltax, (int, float)):
 			raise LatticeError("deltax must be of type float.")
-		if deltax <= 0:
+		if self.deltax <= 0:
 			raise LatticeError("deltax must be greater than 0.")
 		if not isinstance(deltay, (int, float)):
 			raise LatticeError("deltay must be of type float.")
-		if deltay <= 0:
+		if self.deltay <= 0:
 			raise LatticeError("deltay must be greater than 0.")
 		if not isinstance(deltaz, (int, float)):
 			raise LatticeError("deltaz must be of type float.")
-		if deltaz <= 0:
+		if self.deltaz <= 0:
 			raise LatticeError("deltaz must be greater than 0.")
+
+class SphericalLattice(Lattice):
+	"""
+    Spherical lattice class provides a method for creating parts of the spatial
+    	domain within a spherical coordinate system.
+
+    :param center: The center point of the lattice.
+    :type center: float[3] | float(3)
+
+    :param radius: Distance between the center and the surface.
+    :type radius: float
+
+    :param deltas: Distance between two particle on the surface.
+    :type deltas: float
+
+    :param deltar: Radial distance between two particles.
+    :type deltar: float
+
+    :raises LatticeError: if center is not and list, doesn't contain 3 values,
+    	or any value is not a float.
+    """
+    def __init__(self, radius, deltas, center=None, deltar=None):
+    	super().__init__(center)
+
+    	self.radius = radius
+
+    	if deltar is None:
+    		deltar = deltas
+
+    	self.deltas = deltas
+    	self.deltar = deltar
+
+    	self.validate()
+
+    def apply(self, domain, geometry, transform=None, **kwargs):
+    	"""
+    	Fill a domain with particles within the spherical lattice restricted by the geometry.
+
+    	:param domain: Domain particles are to be added to.
+    	:type domain: spatialpy.Domain
+
+    	:param geometry: Geometry defining the region within the lattice in
+    		which particles are restricted to.
+    	:type geometry: spatialpy.Geometry | spatialpy.CombinatoryGeometry
+
+    	:param transform: Transformation function applied to each particle.
+    	:type transform: function
+
+    	:param \**kwargs: Additional keyword arguments passed to :py:meth:`Domain.add_point`.
+    	"""
+    	if not (isinstance(domain, Domain) or type(domain).__name__ == 'Domain'):
+    		raise LatticeError("domain must be of type spatialpy.Domain.")
+    	if not (isinstance(geometry, (Geometry, CombinatoryGeometry)) or \
+    		type(geometry).__name__ in ('Geometry', 'CombinatoryGeometry')):
+    		raise LatticeError(
+    			"geometry must be of type spatialpy.Geometry or spatialpy.CombinatoryGeometry."
+    		)
+    	if transform is not None and not isinstance(transform, 'function'):
+    		raise LatticeError("transform must be a function.")
+
+    	count = 0
+    	radius = self.radius
+    	while radius >= 0:
+    		# Calculate the approximate number of particle with the radius
+    		approx_rc = round((4 * radius ** 2) / ((self.deltas / 2) ** 2))
+
+    		# Set constants for the radius
+    		p_area = 4 * numpy.pi * radius ** 2 / approx_rc
+    		d_a = numpy.sqrt(p_area)
+    		m_phi = round(numpy.pi * radius / d_a)
+    		d_phi = numpy.pi / m_phi
+        	d_theta = p_area / d_phi
+        	
+        	for mphi in range(m_phi):
+        		phi = numpy.pi * (mphi + 0.5) / m_phi
+            	m_theta = round(2 * numpy.pi * numpy.sin(phi) / d_phi)
+            
+            	for mtheta in range(m_theta):
+            		theta = 2 * numpy.pi * mtheta / m_theta
+	                x = radius * numpy.cos(theta) * numpy.sin(phi)
+	                y = radius * numpy.sin(theta) * numpy.sin(phi)
+	                z = radius * numpy.cos(phi)
+	                if geometry.inside((x, y, z), False):
+                        if transform is None:
+                        	point = [x, y, z]
+                        else:
+                        	point = transform([x, y, z])
+                        domain.add_point(point, **kwargs)
+                        count += 1
+            radius -= self.
+        return count
+
+    def validate(self):
+    	"""
+    	Validate the spherical lattice dependencies.
+    	"""
+    	if not isinstance(self.radius, (int, float)):
+    		raise LatticeError("radius must be of type float.")
+    	if self.radius <= 0:
+    		raise LatticeError("radius must be greater than 0.")
+    	if not isinstance(self.deltas, (int, float)):
+    		raise LatticeError("deltas must be of type float.")
+    	if self.deltas <= 0:
+    		raise LatticeError("deltas must be greater than 0.")
+    	if not isinstance(self.deltar, (int, float)):
+    		raise LatticeError("deltar must be of type float.")
+    	if self.deltar <= 0:
+    		raise LatticeError("deltar must be greater than 0.")
