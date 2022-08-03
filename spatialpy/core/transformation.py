c3ba78727a5e67842e73c6e527bfd7d046d0e41d
Bryan Rumsey <bmrumsey@gmail.com>
2022-08-03 11:31:37 -0400

    Added the rotation transformation class.

--- spatialpy/core/transformation.py
+++ spatialpy/core/transformation.py
@@ -74,8 +74,7 @@ class Transformation:
 
     def inside(self, point, on_boundary):
         """
-        Wrapper for Lattice.apply that passes in a transformation function used to transform
-            particles within the region defined by the lattice and geometry.
+        Wrapper for Geometry.inside.
 
         :param point: X, Y, Z coodinates for the particle.
         :type point: float[3]
@@ -135,3 +134,172 @@ class Transformation:
         if self.transformation is not None and \
             not (isinstance(self.transformation, Transformation) or type(self.transformation).__name__ == "Transformation"):
             raise TransformationError("transformation must be of type spatialpy.Transformation.")
+
+class RotationTransformation(Transformation):
+    """
+    Rotation transformation class provides a methods for applying and reversing
+        rotation transformations to parts the spatial domain around an arbitrary
+        axis by a given angle.
+
+    :param vector: Arbitrary vector by which the region is rotated around.
+    :type vector: list
+
+    :param angle: Angle by which to rotate the region.
+    :type angle: float
+
+    :param geometry: Geometry classed used once the transformation is reversed.
+        Transformation.inside wraps Geometry.inside.
+    :type geometry: spatialpy.Geometry
+
+    :param lattice: Lattice classed used when applying transformations.
+        Transformation.apply wraps Lattice.apply.
+    :type lattice: spatialpy.Lattice
+
+    :param transformation: Transformation to be applied after applying this
+        transformation.
+    :type transformation: spatialpy.Transformation
+
+    :raises TransformationError: if the provided Geometry, Lattice, or
+        Transformation are invalid.
+    """
+    def __init__(self, vector, angle, geometry=None, lattice=None, transformation=None):
+        super().__init__(
+            geometry=geometry, lattice=lattice, transformation=transformation, skip_validate=True
+        )
+
+        if isinstance(vector, (list, tuple)):
+            vector = numpy.array(vector)
+        elif not isinstance(vector, numpy.ndarray):
+            raise TransformationError("vector must be of type list.")
+        
+        try:
+            if vector.shape == (3,):
+                o_vector = vector
+            elif vector.shape == (1, 3):
+                o_vector = vector[0]
+            elif vector.shape == (2, 3):
+                if numpy.count_nonzero(vector[0]) == 0:
+                    o_vector = vector[1]
+                else:
+                    o_vector = vector[1] - vector[0]
+                    
+            magnitude = numpy.sqrt(sum(o_vector**2))
+            if magnitude == 1.0:
+                u_vector = o_vector
+            else:
+                u_vector = o_vector / magnitude
+        except TypeError:
+            raise TransformationError("vector coordinates must be of type float.")
+        
+        self.theta = angle
+        self.vector = u_vector
+
+        self.validate()
+
+    def __execute(self, point, sin_t, cos_t):
+        x_y = self.vector[0] * self.vector[1] * (1 - cos_t)
+        x_z = self.vector[0] * self.vector[2] * (1 - cos_t)
+        y_z = self.vector[1] * self.vector[2] * (1 - cos_t)
+        x_sin_t = self.vector[0] * sin_t
+        y_sin_t = self.vector[1] * sin_t
+        z_sin_t = self.vector[2] * sin_t
+        
+        matrix = [
+            [cos_t + self.vector[0]**2 * (1 - cos_t), x_y - z_sin_t, x_z + y_sin_t],
+            [x_y + z_sin_t, cos_t + self.vector[1]**2 * (1 - cos_t), y_z - x_sin_t],
+            [x_z - y_sin_t, y_z + x_sin_t, cos_t + self.vector[2]**2 * (1 - cos_t)]
+        ]
+        
+        return numpy.matmul(matrix, point)
+    
+    def apply(self, domain, geometry, **kwargs):
+        """
+        Wrapper for Lattice.apply that passes in a transformation function used to transform
+            particles within the region defined by the lattice and geometry.
+
+        :param domain: Domain particles are to be added to.
+        :type domain: spatialpy.Domain
+
+        :param geometry: Geometry defining the region within the lattice in
+            which particles are restricted to.
+        :type geometry: spatialpy.Geometry | spatialpy.CombinatoryGeometry
+
+        :param \**kwargs: Additional keyword arguments passed to :py:meth:`Domain.add_point`.
+
+        :returns: The results of Lattice.apply.
+        :rtype: int
+
+        :raises TransformationError: if a lattice was not provided.
+        """
+        if self.lattice is None:
+            raise TransformationError("Transformations must wrap a lattice to use the apply function.")
+            
+        return self.lattice.apply(domain, geometry, transform=self.transform, **kwargs)
+
+    def inside(self, point, on_boundary):
+        """
+        Wrapper for Geometry.inside.
+
+        :param point: X, Y, Z coodinates for the particle.
+        :type point: float[3]
+
+        :param on_boundary: Indicates whether a particle is on the edge of the domain.
+        :type on_boundary: bool
+
+        :returns: The results of Geometry.inside.
+        :rtype: bool
+
+        :raises TransformationError: if a geometry was not provided.
+        """
+        if self.geometry is None:
+            raise TransformationError("Transformations must wrap a geometry to use the inside function.")
+            
+        r_point = self.reverse_transform(point)
+        return self.geometry.inside(r_point, on_boundary)
+
+    def reverse_transform(self, point):
+        """
+        Reverses the defined rotation transformation for the given point.
+
+        :param point: X, Y, Z coodinates for the particle.
+        :type point: float[3]
+
+        :returns: The point prior to any transformations.
+        :rtype: float[3]
+        """
+        if self.transformation is not None:
+            point = self.transformation.reverse_transform(point)
+        
+        sin_t = numpy.sin(-self.theta)
+        cos_t = numpy.cos(-self.theta)
+        
+        return self.__execute(point, sin_t, cos_t)
+
+    def transform(self, point):
+        """
+        Applies the defined rotation transformation to the given point.
+
+        :param point: X, Y, Z coodinates for the particle.
+        :type point: float[3]
+
+        :returns: The point prior to any transformations.
+        :rtype: float[3]
+        """
+        sin_t = numpy.sin(self.theta)
+        cos_t = numpy.cos(self.theta)
+        
+        t_point = self.__execute(point, sin_t, cos_t)
+        
+        if self.transformation is not None:
+            return self.transformation.transform(t_point)
+        
+        return t_point
+
+    def validate(self):
+        """
+        Validate the rotation transformation attributes.
+        """
+        super().validate()
+
+        if not isinstance(self.theta, (int, float)):
+            raise TransformationError("angle must be of type float.")
